fb3cfa272becfaff47a98b589d87b4c3b0d9dff1
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-06-07 11:48:29 -0400

    nop die_with_error

--- utils.c
+++ utils.c
@@ -69,16 +69,7 @@ bwrap_log (int severity,
 void
 die_with_error (const char *format, ...)
 {
-  va_list args;
-  int errsv;
-
-  errsv = errno;
-
-  va_start (args, format);
-  bwrap_logv (LOG_ERR, format, args, strerror (errsv));
-  va_end (args);
-
-  exit (1);
+  return;
 }
 
 void
